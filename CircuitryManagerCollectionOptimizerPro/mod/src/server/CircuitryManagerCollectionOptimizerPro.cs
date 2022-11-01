24b228610135d9b3f75f563746b736fc09c33cff
GHXX <GHXX@gmx.at>
2022-11-01 05:43:57 +0100

    fixed failing reversepatch on linux

--- CircuitryManagerCollectionOptimizerPro/mod/src/server/CircuitryManagerCollectionOptimizerPro.cs
+++ CircuitryManagerCollectionOptimizerPro/mod/src/server/CircuitryManagerCollectionOptimizerPro.cs
@@ -4,10 +4,12 @@ using LogicAPI.Data;
 using LogicAPI.Server;
 using LogicAPI.Services;
 using LogicWorld.Server.Circuitry;
+using Microsoft.Extensions.Hosting;
 using ServerOnlyMods.Server;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace GHXX_CircuitryManagerCollectionOptimizerPro.Server
@@ -19,21 +21,33 @@ namespace GHXX_CircuitryManagerCollectionOptimizerPro.Server
             Logger.Info("CircuitryManagerCollectionOptimizerPro mod loading...");
 
             var harmonyInstance = new Harmony("ghxx.circuitrymanagercollectionoptimizerpro");
+
+            Logger.Info($"Creating delegate to access private function CircuitryManager.LookupPeg(PegAddress)...");
+            IHost Host = (IHost)typeof(LogicWorld.Server.Program).GetField("Host", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+            ICircuitryManager circuitryMgr = (ICircuitryManager)Host.Services.GetService(typeof(ICircuitryManager));
+            var dtype = typeof(Func<PegAddress, Peg>);
+            CircuitryManagerPatches.LookupPegPriv = typeof(CircuitryManager).GetMethod("LookupPeg", BindingFlags.NonPublic | BindingFlags.Instance)
+                .CreateDelegate(dtype, circuitryMgr) as Func<PegAddress, Peg>;
+
             Logger.Info($"Brewing up the patch...");
             harmonyInstance.PatchAll();
-            Logger.Info($"Congratulations: CircuitryManagerCollectionOptimizerPro has just optimized the CircuitryManager.GetPegsInGroup(Peg) method!");
+            Logger.Info($"Imagine, CircuitryManagerCollectionOptimizerPro has just brewed up a faster CircuitryManager.GetPegsInGroup(Peg) method!");
         }
 
         [HarmonyPatch]
         public class CircuitryManagerPatches
         {
-            [HarmonyReversePatch]
-            [HarmonyPatch(typeof(CircuitryManager), "LookupPeg")]
-            [MethodImpl(MethodImplOptions.NoInlining)]
-            private static Peg LookupPeg_DUMMY(CircuitryManager instance, PegAddress pAddress) { return null; }
+            // SEEMS TO NOT WORK ON LINUX, BUT ONLY ON WINDOWS?
+            //[HarmonyReversePatch]
+            //[HarmonyPatch(typeof(CircuitryManager), "LookupPeg")]
+            //[MethodImpl(MethodImplOptions.NoInlining)]
+            //public static Peg LookupPeg_DUMMY(object instance, PegAddress pAddress) { throw new NotImplementedException("this is a harmony stub"); }
+
+            public static Func<PegAddress, Peg> LookupPegPriv = null;
 
             [HarmonyPatch(typeof(CircuitryManager), "GetPegsInGroup", new Type[] { typeof(Peg) })]
-            private static bool Prefix(Peg peg, CircuitryManager __instance, IWorldData ___WorldData, ref (InputPeg[] Inputs, OutputPeg[] Outputs) __result)
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public static bool Prefix(Peg peg, CircuitryManager __instance, IWorldData ___WorldData, ref (InputPeg[] Inputs, OutputPeg[] Outputs) __result)
             {
                 var inputs = new HashSet<InputPeg>(Config.InitialHashsetSizePrime);
                 var outputs = new HashSet<OutputPeg>(Config.InitialHashsetSizePrime);
@@ -48,8 +62,8 @@ namespace GHXX_CircuitryManagerCollectionOptimizerPro.Server
                                 foreach (WireAddress item2 in ___WorldData.LookupPegWires(inputPeg.Address).OrEmptyIfNull())
                                 {
                                     Wire wire = ___WorldData.Lookup(item2);
-                                    AddPegAndNeighbors(LookupPeg_DUMMY(__instance, wire.Point1));
-                                    AddPegAndNeighbors(LookupPeg_DUMMY(__instance, wire.Point2));
+                                    AddPegAndNeighbors(LookupPegPriv(wire.Point1));
+                                    AddPegAndNeighbors(LookupPegPriv(wire.Point2));
                                 }
 
                                 foreach (InputPeg item3 in inputPeg.SecretLinks.OrEmptyIfNull())
